365392398ef378d175738ff0fda17bfd6af5e195
Arin Singh <arinsingh.work@gmail.com>
2025-07-20 05:23:04 +0530

    AddMoney bug

--- apps/user-app/next.config.js
+++ apps/user-app/next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   transpilePackages: ["@repo/ui"],
+  experimental: {
+    staleTimes: {
+      dynamic: 0,
+      static: 0,
+    },
+  },
 };
